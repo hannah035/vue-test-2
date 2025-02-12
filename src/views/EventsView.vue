e495377e809d387545e01a55867efae33aa0fa5e
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-02-12 14:28:33 +0800

    eventsview and added font import

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,11 +1,153 @@
 <template>
+	<link
+		href="https://fonts.googleapis.com/css?family=JetBrains+Mono"
+		rel="stylesheet"
+	/>
 
+	<div class="container">
+		<div class="event-section">
+			<div class="section-title">Events</div>
+			<div class="event-image-container">
+				<div
+					v-for="(event, index) in Events[selectedPeriod]"
+					:key="index"
+					class="event-image"
+					:class="{ hidden: selectedEvent !== index }"
+				>
+					<img :src="event.cover" />
+				</div>
+			</div>
+		</div>
+		<div class="period-dropdown">
+			<button v-on:click="expandDropdown" class="dropdown-button">
+				{{ selectedPeriod }}
+			</button>
+			<div class="dropdown-content">
+				<div
+					v-for="period in timePeriodList"
+					:key="period"
+					v-on:click="selectedPeriod = period"
+					class="dropdown-item"
+				>
+					{{ period }}
+				</div>
+			</div>
+		</div>
+
+		<div class="event-options">
+			<div
+				v-for="(event, index) in Events[selectedPeriod]"
+				:key="index"
+				class="event-item"
+				v-on:mouseover="selectedEvent = index"
+				:class="{ highlight: selectedEvent === index }"
+			>
+				{{ event.title }}
+			</div>
+		</div>
+	</div>
 </template>
 
-<script setup>
-
+<script>
+import eventsData from "@/assets/EventsData/Events"
+export default {
+	name: "EventsView",
+	data() {
+		const timePeriodList = Object.keys(eventsData)
+		return {
+			timePeriodList,
+			selectedPeriod: timePeriodList[0],
+			Events: eventsData,
+			selectedEvent: 0,
+		}
+	},
+	methods: {
+		expandDropdown() {
+			document
+				.querySelector(".dropdown-content")
+				.classList.toggle("expand")
+		},
+	},
+}
 </script>
 
 <style scoped>
+.container {
+	width: 100%;
+	height: 100%;
+	display: grid;
+	box-sizing: border-box;
+	grid-template-columns: 77% 1fr;
+	column-gap: 4%;
+	grid-template-rows: repeat(2, 1fr);
+	padding: 4.5% 5%;
+}
 
+.event-section {
+	grid-row: span 2;
+	border-radius: 15px;
+	background: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(10px);
+	padding: 4.5%;
+}
+.section-title {
+	color: #fff;
+	font-family: "JetBrains Mono";
+	font-size: 20px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+	position: relative;
+}
+
+.event-image img {
+	position: relative;
+	top: 50px;
+	width: 100%;
+}
+.hidden {
+	display: none;
+}
+/* TODO dropdown button and content position */
+ .period-dropdown {
+	height: 100%;
+	width: 100%;
+	/* background-color: green; */
+}
+.dropdown-button {
+	width: 100%;
+	background-color: transparent;
+	background-repeat: no-repeat;
+	border: none;
+	display: inline-block;
+	color: #fff;
+	font-family: "JetBrains Mono";
+	font-size: medium;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+}
+.period-dropdown .dropdown-content {
+	display: none;
+	color: #fff;
+	font-family: "JetBrains Mono";
+	font-size: medium;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+	text-align: center;
+}
+.period-dropdown .dropdown-content.expand {
+	display: block;
+}
+
+/* TODO event-item position and style */
+.event-item {
+	font-size: small;
+}
+
+.highlight {
+	color: yellow;
+	/* TODO change highlight color */
+}
 </style>
