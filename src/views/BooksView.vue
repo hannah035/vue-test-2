9567b5019d8308c7b4eb39d9936fa9e0785c956c
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-02-16 16:08:18 +0800

    booksView added

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -1,33 +1,26 @@
 <template>
 	<!-- bookshelf.html -->
-	{% extends "layout.html" %} {% block content %}
-
-	<div class="grid-container" id="split-container">
-		<div class="left-side" id="left-side">
+	<div class="handleScroll">
+		<div class="grid-container" id="split-container">
 			<section id="bookshelf">
 				<div class="books-container">
-
-
-
 					<div class="product-list">
-						{% for book in books%}
-
 						<button
 							class="product-card"
+							v-for="(book, index) in booksData"
+							:key="book"
 						>
 							<div class="product-thumbnail">
-								<img loading="lazy" />
+								<img loading="lazy" :src="book.cover" />
 							</div>
-							<div class="product-title-wrap">
-								<div class="product-title">
-									book name
-								</div>
+
+							<div class="product-title">
+								<div class="text">{{ book.title }}</div>
 							</div>
 						</button>
-						{% endfor %}
 					</div>
 
-					<div class="pagination-container">
+					<!-- <div class="pagination-container">
 						<div class="pagination">
 							{% if page_current != page_start %}
 							<a
@@ -81,7 +74,7 @@
 							<a
 								class="pagination-number active"
 								href="{{ url_for('bookshelf', page=page_current) }}"
-								>{{ page_current }}</a
+						s		>{{ page_current }}</a
 							>
 							<a
 								class="pagination-number"
@@ -96,112 +89,174 @@
 							>
 							{% endif %}
 						</div>
-					</div>
-
-					<div class="footer" id="footer">
-						<div class="footer-inner">
-							<div class="footer-section">
-								<div class="footer-title">
-									臺灣大學星艦學院科幻社
-								</div>
-								<div class="footer-address">
-									第二學生活動中心
-									<br />
-									9F 921
-									<br />
-									ntusfa@gipo.ntu.edu.tw
-								</div>
-							</div>
-							<div class="footer-section">
-								<div class="footer-links-title">Links</div>
-								<div class="footer-link">IG</div>
-								<div class="footer-link">FB</div>
-								<div class="footer-link">About</div>
-								<div class="footer-link">Contact</div>
-							</div>
-							<div class="footer-section">
-								<div class="footer-help-title">Join Us</div>
-								<div class="footer-help-link">入社表單</div>
-								<div class="footer-help-link">社團規則</div>
-								<div class="footer-help-link">活動報名</div>
-							</div>
-							<div class="footer-section">
-								<div class="footer-newsletter-title">
-									Say Anything
-								</div>
-								<div class="footer-newsletter">
-									<div class="newsletter-prompt">
-										講師推薦、意見回饋......等
-									</div>
-									<div class="newsletter-divider"></div>
-									<div class="newsletter-submit">
-										Transmit
-									</div>
-									<div class="newsletter-divider"></div>
-								</div>
-							</div>
-						</div>
-						<div class="footer-divider"></div>
-						<div class="footer-copyright">2024summer</div>
-					</div>
+					</div> -->
 				</div>
 			</section>
-		</div>
-		<div class="right-side initial" id="right-side">
-			<!-- Embed pop-up.html here -->
-			<iframe
-				src="{{ url_for('side') }}"
-				frameborder="0"
-				onload="split_close_function()"
-				id="iframe"
-			></iframe>
 		</div>
 	</div>
 </template>
 <script>
-const splitContainer = document.getElementById("split-container")
-const right = document.getElementById("right-side")
-const left = document.getElementById("left-side")
-let iframe
-let innerDoc
-function split_close_function() {
-	setTimeout(() => {
-		right.classList.remove("initial")
-	}, 200)
-	iframe = document.getElementById("iframe")
-	innerDoc = iframe.contentDocument || iframe.contentWindow.document
-	const pushBack = innerDoc.getElementById("push-back")
-	pushBack.addEventListener("click", () => {
-		left.classList.remove("appear")
-		right.classList.remove("appear")
-	})
-}
-function set_split_view_data(
-	isbn,
-	name,
-	author,
-	publisher,
-	date,
-	cover,
-	place,
-	borrow
-) {
-	// set innerHTML of side.html
-	iframe = document.getElementById("iframe")
-	innerDoc = iframe.contentDocument || iframe.contentWindow.document
-	innerDoc.getElementById("book-title").innerHTML = String(name)
-	innerDoc.getElementById("book-details").innerHTML = `${isbn.substring(
-		5
-	)}/${place}/可借閱`
-	innerDoc.getElementById(
-		"book-description"
-	).innerHTML = `作者 ${author} <br/>出版社 ${publisher}<br/> 出版年份 ${date}`
-	innerDoc.getElementById("book-cover-img").src = cover
-	innerDoc.getElementById("book-id").value = String(isbn)
-
-	//make right appear and left shrink
-	right.classList.add("appear")
-	left.classList.add("appear")
+import booksData from "@/assets/BooksData/Books.json"
+export default {
+	name: "BooksView",
+	data() {
+		return {
+			booksData,
+			booksKey: Object.keys(booksData),
+			page: 1,
+		}
+	},
+	methods: {},
 }
 </script>
-<style></style>
+<style>
+:root {
+	--search_bar_height: 2vw;
+	--animation-duration: 0.7s;
+}
+a {
+	all: unset;
+}
+body {
+	overflow-y: hidden;
+}
+.handleScroll {
+	width: 100%;
+	height: 100%;
+	overflow-x: hidden;
+	overflow-y: scroll;
+}
+.books-container {
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+}
+
+/*books ----------------------------------------------------------------------------------- */
+.product-list {
+	display: grid;
+	grid-template-columns: repeat(
+		auto-fit,
+		minmax(15rem, 1fr)
+	); /* Automatically adjusts the number of columns */
+	gap: 2vw 3vw; /* Horizontal and vertical space */
+	justify-content: center; /* Center the grid within the container */
+	width: 100%;
+	padding: 4vw 5vw; /* Padding */
+	box-sizing: border-box; /* Includes padding in the element's width/height */
+}
+button {
+	all: unset;
+}
+.product-card {
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 80% 20%;
+	background: rgba(255, 255, 255, 0.2);
+	padding: 1vw; /* Padding inside the card */
+	text-align: center;
+	box-shadow: 0 0.3vw 0.6vw rgba(0, 0, 0, 0.1); /* Softer shadow */
+	border-radius: 0.5vw; /* Smoother corners */
+	transition: transform 0.3s ease, box-shadow 0.3s ease; /* Hover effects */
+	overflow: hidden; /* Ensure content stays within bounds */
+	aspect-ratio: 3 / 4; /* Maintain consistent card aspect ratio */
+	margin-bottom: 1vw; /* Reduced margin-bottom for smaller space under the card */
+	text-decoration: none; /* Remove underline from title */
+	position: relative;
+}
+
+.product-card:hover {
+	transform: translateY(-0.5vw); /* Lift on hover */
+	box-shadow: 0 0.5vw 1vw rgba(0, 0, 0, 0.2); /* Enhanced shadow on hover */
+}
+
+.product-thumbnail {
+	width: 85%; /* 85% of the card width */
+	aspect-ratio: 1 / 1;
+	overflow: hidden;
+	margin: 1vw 0; /* Space around the thumbnail */
+	border-radius: 0.5vw; /* Rounded corners */
+	flex-shrink: 0; /* Prevent shrinking */
+	position: relative;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
+.product-thumbnail img {
+	width: 100%;
+	height: 100%;
+	object-fit: contain;
+	max-width: 100%; /* Constrain the image's width to the parent div */
+	max-height: 100%; /* Constrain the image's height to the parent div */
+}
+
+.product-title {
+	width: 100%;
+	margin-bottom: 0; /* Remove bottom margin */
+	padding: 0; /* Remove padding */
+	font-size: 1rem; /* Font size based on rem */
+	font-weight: 500;
+	color: white; /* Slightly darker text for better readability */
+	line-height: 1.4; /* Improved line-height for readability */
+	text-transform: capitalize; /* Capitalize each word */
+	position: relative;
+}
+.product-title .text {
+	top: 50%;
+	position: relative;
+	transform: translateY(-50%);
+}
+
+/*pagination ---------------------------------------------------------------------------------------------------- */
+.pagination-container {
+	display: flex;
+	justify-content: center;
+	width: 100%;
+}
+
+.pagination {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 2rem;
+	padding-top: 3vh;
+	font-size: 1.6rem;
+	font-weight: 400;
+	max-width: 100%;
+}
+.pagination a {
+	display: block;
+	text-decoration: none;
+}
+
+@media (max-width: 991px) {
+	.pagination {
+		margin-top: 4vh;
+	}
+}
+
+.pagination-number,
+.pagination-np {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 1rem 2rem;
+	border-radius: 1rem;
+	background-color: rgba(249, 241, 231, 1);
+	color: black;
+	cursor: pointer;
+}
+
+.pagination-number.active {
+	background-color: #b88e2f;
+	color: white;
+}
+
+@media (max-width: 991px) {
+	.pagination-number,
+	.pagination-next {
+		padding: 0.8rem 1.6rem;
+		white-space: initial;
+	}
+}
+</style>
