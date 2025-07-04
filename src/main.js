624332a0db839d347945a55cc17c86cf0b25f8ff
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-07-04 21:39:24 +0800

    add console config

--- src/main.js
+++ src/main.js
@@ -5,4 +5,7 @@ import router from "./router"
 
 const app = createApp(App)
 app.use(router)
+// Add for console access in Vue components
+// This allows you to use `console.log` in your Vue components
+app.config.globalProperties.console = console;
 app.mount("#app")
